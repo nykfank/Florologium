3c91953c963560e5259f87e2438ffdd64aa08a6a
nykfank <nick@nyk.ch>
2022-05-08 00:28:14 +0200

    Add files via upload

--- nikon_pic.sh
+++ nikon_pic.sh
@@ -1,8 +1,8 @@
 #!
 filename="$(date +"%Y%m%d_%H%M%S").jpg"
-su pi -c "ssh flortun rm capt0000.jpg"
-su pi -c "ssh flortun gphoto2 --capture-image-and-download --force-overwrite"
-su pi -c "rsync flortun:capt0000.jpg /var/www/florologium/nikon/$filename"
-su pi -c "/usr/bin/convert -geometry 696x464 /var/www/florologium/nikon/$filename /var/www/florologium/nikon_current.jpg"
+ssh flortun rm capt0000.jpg
+ssh flortun gphoto2 --capture-image-and-download --force-overwrite
+rsync flortun:capt0000.jpg /var/www/florologium/nikon/$filename
+/usr/bin/convert -geometry 696x464 /var/www/florologium/nikon/$filename /var/www/florologium/nikon_current.jpg
 /home/pi/boga/nikon_publish.py /var/www/florologium/nikon/$filename
 
--- nikon_publish.py
+++ nikon_publish.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys, subprocess, re, os, time
 filename = sys.argv[1]
+html_path = '/var/www/florologium/content_foto.html'
 if not os.path.isfile(filename): 
 	print('File does not exist: %s' % filename)
 	sys.exit()
-xml_path = '/var/www/florologium/data/pages'
-xml_filelist = ['index.xml', 'index_fr.xml', 'index_de.xml']
 cmd = 'exiftool', filename
 r = subprocess.check_output(cmd)
 r = r.decode('ascii')
@@ -16,21 +15,6 @@ exp_time = re.sub(' +', ' ', exp_time)
 exp_time = exp_time.replace(' :', ':')
 zeit = os.path.splitext(os.path.basename(filename))[0]
 zeit2 = '%s-%s-%s %s:%s:%s' % (zeit[0:4], zeit[4:6], zeit[6:8], zeit[9:11], zeit[11:13], zeit[13:15])
-
-html_text = """<!--MARKER_START-->
-<p><img alt="Current picture of Florologium" src="https://www.florologium.ch/nikon_current.jpg?%d" style="width: 696px; height: 464px;" />
-<br/>%s, %s s</p>
-<!--MARKER_END-->""" % (time.time(), zeit2, exp_time)
-
-def insert_html_in_CMS(html_text, xml_file):
-	html_text = html_text.replace('<', '&lt;')
-	html_text = html_text.replace('>', '&gt;')
-	html_text = html_text.replace('"', '&quot;')
-	xml_data = open(xml_file).read()
-	xml1 = xml_data.split('&lt;!--MARKER_START--&gt;')[0]
-	xml2 = xml_data.split('&lt;!--MARKER_END--&gt;')[1]
-	xml_new = xml1 + html_text + xml2
-	open(xml_file, 'w').write(xml_new)
-
-for xml_file in xml_filelist:
-	insert_html_in_CMS(html_text, '%s/%s' % (xml_path, xml_file))
+html_text = """<p><img alt="Current picture of Florologium" src="https://www.florologium.ch/nikon_current.jpg?%d" style="width: 696px; height: 464px;" />
+<br/>%s, %s s</p>""" % (time.time(), zeit2, exp_time)
+open(html_path, 'w').write(html_text)
