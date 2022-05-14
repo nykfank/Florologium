2aed6ba2c0fbad0b83fb1ab8f1d7e0cd3e07659f
nykfank <nick@nyk.ch>
2022-05-15 00:13:52 +0200

    Add files via upload

--- wan_update.sh
+++ wan_update.sh
@@ -1,7 +1,7 @@
 #!
 cd ~/wan
 curl -s ifconfig.me >address.txt
-git add address.txt 
-git commit -m "Update"
+git add address.txt
+git commit -m "Update" --quiet
 git push https://nykfank:GITHUB_PUBLIC_ACCESS_KEY@github.com/nykfank/wan.git --all
 
