45574cc1f6149a4486bb4fea96bc10c8f0990eb0
nykfank <nick@nyk.ch>
2022-05-16 23:23:14 +0200

    Add files via upload

--- nikon_pic.sh
+++ nikon_pic.sh
@@ -2,28 +2,33 @@
 filename="$(date +"%Y%m%d_%H%M%S").jpg"
 ssh flortun "test -e capt0000.jpg"
 if [ $? -eq 0 ]; then
-    ssh flortun rm capt0000.jpg
+	ssh flortun rm capt0000.jpg
 fi
 ssh flortun gphoto2 --capture-image-and-download --force-overwrite
 ssh flortun "test -e capt0000.jpg"
 if [ ! $? -eq 0 ]; then
-    echo 'No foto taken.'
-    exit
+	echo 'No foto taken.'
+	exit
+fi
+filesize=$(ssh flortun stat -c%s "capt0000.jpg")
+if [[ $filesize -lt 10000000 ]]; then
+	echo 'Filesize below 10MB, use bulb mode!'
+	ssh flortun gphoto2 --set-config iso=8000 --capture-image-and-download --force-overwrite --bulb 30
 fi
 rsync flortun:capt0000.jpg /var/www/florologium/nikon/$filename
 if [[ ! -f /var/www/florologium/nikon/$filename ]] ; then
-    echo 'No file downloaded from flortun.'
-    exit
+	echo 'No file downloaded from flortun.'
+	exit
 fi
 ssh flor "test -e /home/nyk/nikon_current.jpg"
 if [ $? -eq 0 ]; then
-    ssh flor rm /home/nyk/nikon_current.jpg
+	ssh flor rm /home/nyk/nikon_current.jpg
 fi
 ssh flor convert -geometry 696x464 /home/nyk/nikon/$filename /home/nyk/nikon_current.jpg
 ssh flor "test -e /home/nyk/nikon_current.jpg"
 if [ ! $? -eq 0 ]; then
-    echo 'Failed to convert image.'
-    exit
+	echo 'Failed to convert image.'
+	exit
 fi
 rsync flor:/home/nyk/nikon_current.jpg /var/www/florologium/nikon_current.jpg
 /home/pi/boga/nikon_publish.py $filename
