6760153ad3c1ef25c4e883b2917f955acbd420b2
nick@nyk.ch <nick@nyk.ch>
2022-05-18 00:20:36 +0200

    Update

--- nikon_pic.sh
+++ nikon_pic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 filename="$(date +"%Y%m%d_%H%M%S").jpg"
 ssh flortun "test -e capt0000.jpg"
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; 
+then
 	ssh flortun rm capt0000.jpg
 fi
 bulbmode=$(~/boga/bulb_mode.py)
@@ -12,22 +13,26 @@ else
     ssh flortun gphoto2 --set-config iso=8000 --capture-image-and-download --force-overwrite --bulb 30
 fi
 ssh flortun "test -e capt0000.jpg"
-if [ ! $? -eq 0 ]; then
+if [[ ! $? -eq 0 ]]; 
+then
 	echo 'No foto.'
 	exit
 fi
 rsync flortun:capt0000.jpg /var/www/florologium/nikon/$filename
-if [[ ! -f /var/www/florologium/nikon/$filename ]] ; then
+if [[ ! -f /var/www/florologium/nikon/$filename ]]; 
+then
 	echo 'No file downloaded from flortun.'
 	exit
 fi
 ssh flor "test -e nikon_current.jpg"
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; 
+then
 	ssh flor rm ~/nikon_current.jpg
 fi
 ssh flor convert -geometry 696x464 nikon/$filename nikon_current.jpg
 ssh flor "test -e nikon_current.jpg"
-if [ ! $? -eq 0 ]; then
+if [[ ! $? -eq 0 ]]; 
+then
 	echo 'Failed to convert image.'
 	exit
 fi
