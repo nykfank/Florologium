0784c644b5ff07e7d992ba51bdbe74518aa1902c
nykfank <nick@nyk.ch>
2022-05-18 00:10:11 +0200

    Add files via upload

--- bulb_mode.py
+++ bulb_mode.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# Returns auto when the sun is up and bulb when it is down.
+# Returns 1 after dusk and before dawn, otherwise 1.
 import astral, astral.sun, datetime
-loc = astral.LocationInfo(timezone='Europe/Zurich', latitude=46.94809, longitude=7.44744)
+loc = astral.LocationInfo(timezone='Europe/Zurich', latitude=46.95263, longitude=7.44545)
 s = astral.sun.sun(loc.observer, tzinfo=loc.timezone)
 current_time = datetime.datetime.now().time()
 dawn_time = s["dawn"].time()
 dusk_time = s["dusk"].time()
-mode = 0
-if current_time < dawn_time or current_time > dusk_time: mode = 1
+mode = 1 if current_time < dawn_time or current_time > dusk_time else 0
 print(mode)
