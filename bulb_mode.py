46851ab4eb1215dba28928631be478a1ae089fcf
nick@nyk.ch <nick@nyk.ch>
2022-05-18 17:40:48 +0200

    Update

--- bulb_mode.py
+++ bulb_mode.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Returns 1 after dusk and before dawn, otherwise 1.
+# Returns 1 after dusk and before dawn, otherwise 0.
 import astral, astral.sun, datetime
 loc = astral.LocationInfo(timezone='Europe/Zurich', latitude=46.95263, longitude=7.44545)
 s = astral.sun.sun(loc.observer, tzinfo=loc.timezone)
 current_time = datetime.datetime.now().time()
 shift = datetime.timedelta(minutes=20)
-dawn_time = s["dawn"].time() - shift
-dusk_time = s["dusk"].time() + shift
+dawn_time = (s["dawn"] - shift).time()
+dusk_time = (s["dusk"] + shift).time()
 mode = 1 if current_time < dawn_time or current_time > dusk_time else 0
 print(mode)
