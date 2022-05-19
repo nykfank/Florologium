d0cbfdbda94db0c485909d5ba04ff1e9cf5515b5
nick@nyk.ch <nick@nyk.ch>
2022-05-20 00:32:59 +0200

    Update

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ rsync -vp ~/Florologium/nikon_publish.py katz:boga
 rsync -vp ~/Florologium/boga_timelapse.sh katz:boga
 rsync -vp ~/Florologium/exposure_timelapse.R flor:bin
 rsync -vp ~/Florologium/make_timelapse.py flor:bin
-rsync -vp ~/Florologium/wan_update.sh katz:boga
+#rsync -vp ~/Florologium/wan_update.sh katz:boga
