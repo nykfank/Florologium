d87f6d6af33ced91915439f79ba7490e906c0d19
nick@nyk.ch <nick@nyk.ch>
2022-05-30 01:47:49 +0200

    Update

---
+++ perspective_transformation.py
@@ -0,0 +1,17 @@
+import cv2
+from operator import itemgetter
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+paper = cv2.imread('nikon_current.jpg')
+# Coordinates that you want to Perspective Transform
+pts1 = np.float32([[1,136],[348,1],[320,464],[696,205]])
+# Size of the Transformed Image
+pts2 = np.float32([[0,0],[696,0],[0,464],[696,464]])
+for val in pts1:
+    cv2.circle(paper,(val[0],val[1]),5,(0,255,0),-1)
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(paper,M,(696,464))
+#plt.imshow(dst)
+cv2.imwrite("foo.jpg", dst)
