9a23923353455dae87aeef56806e0f6dc4bf7a08
nick@nyk.ch <nick@nyk.ch>
2022-05-31 01:21:07 +0200

    Update

--- perspective_transformation.py
+++ perspective_transformation.py
@@ -3,9 +3,9 @@ import cv2, numpy, sys
 mult = 8 # Size multiplier [8 = full, 1 = scaled for web].
 sx = 696*mult # Size horizontal
 sy = 464*mult # Size vertical
-d1 = 150*mult # Top trapezoid points distance to border.
+d1 = 200*mult # Top trapezoid points distance to border.
 d2 = 250*mult # Shift outside image.
-d3 = 120*mult # Crop distance.
+d3 = 100*mult # Crop distance.
 
 paper = cv2.imread(sys.argv[1])
 pts1 = numpy.float32([[d1-d2, 0], [sx-d1+d2, 0], [-d2, sy], [sx+d2, sy]])
