2f58555675bd8a7928de0dc5f9c11f5a840b18c1
nick@nyk.ch <nick@nyk.ch>
2022-05-31 01:38:31 +0200

    Update

--- perspective_transformation.py
+++ perspective_transformation.py
@@ -11,6 +11,6 @@ paper = cv2.imread(sys.argv[1])
 pts1 = numpy.float32([[d1-d2, 0], [sx-d1+d2, 0], [-d2, sy], [sx+d2, sy]])
 pts2 = numpy.float32([[0, 0], [sx, 0], [0, sy], [sx, sy]])
 M = cv2.getPerspectiveTransform(pts1, pts2)
-dst = cv2.warpPerspective(paper, M, (sx,sy), cv2.BORDER_TRANSPARENT)
+dst = cv2.warpPerspective(paper, M, (sx,sy), flags=cv2.INTER_LINEAR, borderValue=(32,53,33))
 crop_img = dst[0:sy, d3:sx-d3]
 cv2.imwrite(sys.argv[2], crop_img)
