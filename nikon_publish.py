d3bc3df548f026a142911e50bdae4946be63ecff
Nick Fankhauser <nick@nyk.ch>
2022-05-31 23:07:23 +0200

    Update

--- nikon_publish.py
+++ nikon_publish.py
@@ -12,11 +12,10 @@ exp_time = re.sub(' +', ' ', exp_time)
 exp_time = exp_time.replace(' :', ':')
 zeit = os.path.splitext(os.path.basename(filename))[0]
 zeit2 = '%s-%s-%s %s:%s' % (zeit[0:4], zeit[4:6], zeit[6:8], zeit[9:11], zeit[11:13])
-html_text = """<p>
-<img alt="Current picture of Florologium" src="https://www.florologium.ch/nikon_current.jpg?%d" style="width: 696px; height: 464px;" />
-<br/>%s, %s s</p>
-<p>
-<img alt="Transformed picture of Florologium" src="https://www.florologium.ch/nikon_current795t.jpg?%d" style="width: 696px; height: 464px;" />
-<br/>Image resulting from perspective transformation.</p>
+html_text = """<p><img alt="Current picture of Florologium" src="https://www.florologium.ch/nikon_current.jpg?%d" style="width: 696px; height: 464px;" />
+<br/>%s, %s s</p><p>
+<img alt="Transformed picture of Florologium" src="https://www.florologium.ch/nikon_current795t.jpg?%d" style="width: 569px; height: 530px;" />
+<br/>Image resulting from perspective transformation.
+</p>
 """ % (time.time(), zeit2, exp_time)
 open(html_path, 'w').write(html_text)
