4bf5a9866efaa50d513c19afccc17b7991326fda
Nick Fankhauser <nick@nyk.ch>
2022-05-31 22:57:16 +0200

    Update

--- upload.sh
+++ upload.sh
@@ -1,12 +1,13 @@
 #!
-rsync -vp ~/Florologium/nikon_pic.sh katz:boga
-rsync -vp ~/Florologium/bulb_mode.py katz:boga
-rsync -vp ~/Florologium/nikon_publish.py katz:boga
-rsync -vp ~/Florologium/boga_timelapse.sh katz:boga
-rsync -vp ~/Florologium/exposure_timelapse.R flor:bin
-rsync -vp ~/Florologium/make_timelapse.py flor:bin
-rsync -vp ~/Florologium/missing.R flor:bin
-rsync -vp ~/Florologium/run_missing.sh katz:boga
-rsync -vp ~/Florologium/crontab_katz.txt katz:.
+rsync -p ~/Florologium/nikon_pic.sh katz:boga
+rsync -p ~/Florologium/bulb_mode.py katz:boga
+rsync -p ~/Florologium/nikon_publish.py katz:boga
+rsync -p ~/Florologium/boga_timelapse.sh katz:boga
+rsync -p ~/Florologium/exposure_timelapse.R flor:bin
+rsync -p ~/Florologium/make_timelapse.py flor:bin
+rsync -p ~/Florologium/missing.R flor:bin
+rsync -p ~/Florologium/run_missing.sh katz:boga
+rsync -p ~/Florologium/crontab_katz.txt katz:.
+rsync -p ~/Florologium/perspective_transformation.py flor:bin
 
 #rsync -vp ~/Florologium/wan_update.sh katz:boga
