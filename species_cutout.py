cc18eb13aa5412a0cf351272778d9ee61792098e
nick@nyk.ch <nick@nyk.ch>
2022-06-08 00:44:19 +0200

    Update

--- species_cutout.py
+++ species_cutout.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import cv2, numpy, sys, os
 data_file = "%s/Florologium/plant_positions.txt" % os.environ['HOME']
+outdir = '%s/cutout' % os.environ['HOME']
 xsize = 320
 ysize = 240
 img = cv2.imread(sys.argv[1])
+if not os.path.isdir(outdir): os.mkdir(outdir)
 
 specd = {}
 for n, i in enumerate(open(data_file).readlines()):
@@ -13,5 +15,5 @@ for n, i in enumerate(open(data_file).readlines()):
 
 for Species_Name, (X_Coordinate, Y_Coordinate, Start_hour, End_hour) in specd.items():
     crop_img = img[Y_Coordinate:Y_Coordinate+ysize, X_Coordinate:X_Coordinate+xsize]
-    outfilename = '%s/cutout/%s.jpg' % (os.environ['HOME'], Species_Name)
+    outfilename = '%s/%s.jpg' % (outdir, Species_Name)
     cv2.imwrite(outfilename, crop_img)
