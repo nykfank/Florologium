eba28e8c29d5152dd3fbc2bcbe264051c176007d
nick@nyk.ch <nick@nyk.ch>
2022-06-15 00:35:41 +0200

    Update

--- make_timelapse.py
+++ make_timelapse.py
@@ -18,8 +18,12 @@ if not os.path.isdir(outdir):
 else:
 	for f in os.listdir(outdir): os.unlink('%s/%s' % (outdir, f))
 selected_files = files[start_index:]
-if nb_days > 1:	del selected_files[::2] # delete every second element
-if nb_days > 7:	del selected_files[::2] # delete every second element
+if nb_days > 1:	
+	del selected_files[::2] # delete every second element
+	del selected_files[::2] # delete every second element
+if nb_days > 7:
+	del selected_files[::2] # delete every second element
+	del selected_files[::2] # delete every second element
 print('Using %d of %d images.' % (len(selected_files), len(files)))
 for f in selected_files: os.symlink('%s/%s' % (indir, f), '%s/%s' % (outdir, f))
 cmd = "ffmpeg -y -hide_banner -loglevel panic -framerate %d -pattern_type glob -i '%s/*.jpg' -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 %s" % (fps, outdir, vidfile)
