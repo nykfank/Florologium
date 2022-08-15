7c061ba7afe208695b022c91f175a2e1e34e3cea
nick@nyk.ch <nick@nyk.ch>
2022-08-16 00:07:15 +0200

    Update

--- timepoint_timelapse_hires.R
+++ timepoint_timelapse_hires.R
@@ -1,11 +1,12 @@
 args <- commandArgs(trailingOnly=TRUE)
-start_hour <- 16
+sel_hour <- 16
 movie_seconds <- 30
 fps <- 20
+photo_interval_seconds <- 5
 nb_imgs <- movie_seconds * fps
 indir <- '/home/nyk/backup_nikon'
-outdir <- sprintf('/home/nyk/florologium_hires_%d', start_hour)
-vidfile <- sprintf('/home/nyk/video/florologium_hires_%d.mp4', start_hour)
+outdir <- sprintf('/home/nyk/florologium_hires_%d', sel_hour)
+vidfile <- sprintf('/home/nyk/florologium_hires_%d.mp4', sel_hour)
 if (!dir.exists(outdir)) {
 	dir.create(outdir)
 } else {
@@ -19,12 +20,17 @@ et$hour <- as.integer(strftime(et$timestamp, "%H"))
 et$minute <- as.integer(strftime(et$timestamp, "%M"))
 et <- et[order(et$timestamp),]
 nb_days <- length(unique(et$date))
+img_per_day <- nb_imgs / nb_days
+et$hourmin <- et$hour + et$minute/60
+et$seldiff <- abs(et$hourmin - sel_hour)
+diff_limit <- img_per_day * photo_interval_seconds / 60 / 2
+subet <- et[et$seldiff <= diff_limit,]
+for (f in subet$filename) {
+	file.symlink(sprintf("%s/%s", indir, f), sprintf("%s/%s", outdir, f))
+}
 
-#for (f in subet$filename) {
-#	file.copy(sprintf("%s/%s", indir, f), sprintf("%s/%s", outdir, f))
-#}
-
-#cmd <- sprintf("ffmpeg -y -hide_banner -loglevel panic -framerate %d -pattern_type glob -i '%s/*.jpg' -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 %s", fps, outdir, vidfile)
-#writeLines(cmd)
-#system(cmd)
+# Use a resolution of 3840 x 2160 (the 4K norm), not the full 5568x3712 of the camera, otherwise it'll be a huge video file.
+cmd <- sprintf("ffmpeg -y -framerate %d -pattern_type glob -i '%s/*.jpg' -s 3840x2160 -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 %s", fps, outdir, vidfile)
+writeLines(cmd)
+system(cmd)
 
