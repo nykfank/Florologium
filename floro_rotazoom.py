24216ca25a48b1748c3a7aeefca233949dad1f63
nick@nyk.ch <nick@nyk.ch>
2022-08-22 00:45:58 +0200

    Update

--- floro_rotazoom.py
+++ floro_rotazoom.py
@@ -5,11 +5,8 @@
 import sys,os,time,Image,math,progressbar,katzlib,numpy,subprocess
 
 framerate=20
-#wink = math.pi
-#wink = 0
 wink = math.pi / 2
 dvdx,dvdy = 640, 480 # target resolution (DVD: 720,576)
-mirr_angle,cutfakt=30,1.53 # try1
 
 starttime=time.time()
 default_widgets=[progressbar.Percentage(), ' ', progressbar.Bar(), progressbar.ETA()]
@@ -34,9 +31,8 @@ def check_temp(d):
 check_temp(tDir)
 photoList=filter(lambda x : x[-4:].lower()=='.jpg',katzlib.scan_directory(startDir,False))
 img=Image.open(photoList[0])
-maxzoom=img.size[0]/2.0/dvdx
 xc,yc=img.size[0]/2,img.size[1]/2 # center of circle
-rad=min(img.size)/2-min([dvdx*maxzoom,dvdy*maxzoom])/2 # circle radius
+rad=min(img.size)/2-min([dvdx,dvdy])/2 # circle radius
 ely=(float(img.size[0])/float(img.size[1]))*0.95 # elyptic factor
 wg = 2*math.pi/len(photoList)
 
@@ -45,9 +41,6 @@ txt+='Source resolution: %d x %d, \n' % (img.size[0],img.size[1])
 txt+='Target resolution: %d x %d, \n' % (dvdx,dvdy)
 txt+='Real length: %d seconds, \n' % (len(photoList)/framerate)
 txt+='Circle: r = %d, M = %d / %d, ely = %2.2f, v = %2.3f, wink = %2.2f, \n' % (rad,xc,yc,ely,wg,wink)
-txt+='MaxZoom = %2.2f, MinZoom = %2.2f, \n' % (maxzoom,minzoom)
-txt+='ZoomSteps = %d, sum(zoomlist) = %2.2f, \n' % (len(zoomlist),sum(zoomlist))
-if do_rotamirr: txt+='Mirror: %s, Mirror Angle = %d, cutfakt = %2.2f.\n' % (do_rotamirr,mirr_angle,cutfakt)
 txt+='Temp dir: %s,\n' % tDir
 print txt
 
