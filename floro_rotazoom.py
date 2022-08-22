b6b4e11e5c4671192fd6d527d22c09813154e3e4
nick@nyk.ch <nick@nyk.ch>
2022-08-23 00:59:59 +0200

    Update

--- floro_rotazoom.py
+++ floro_rotazoom.py
@@ -2,38 +2,45 @@
 # Rotating zoom timelapse
 # 2010-06-21 by Nick Fankhauser
 # 2014-03-24: simplified
-import sys,os,time,Image,math,progressbar,katzlib,numpy,subprocess
+# 2020-08-22: Adapted to python3
+import sys,os,time,Image,math,numpy
 
-framerate=20
+framerate = 20
 wink = math.pi / 2
-dvdx,dvdy = 640, 480 # target resolution (DVD: 720,576)
-
+wink = math.pi 
+dvdx,dvdy = 2048, 1080 # target resolution (DVD: 720,576)
 starttime=time.time()
-default_widgets=[progressbar.Percentage(), ' ', progressbar.Bar(), progressbar.ETA()]
 startDir=os.path.abspath(sys.argv[1]).rstrip('/')+'/'
 tDir ='%s_rota' % startDir.rstrip('/')
 
 def clean_directory(d):
- pbar = progressbar.ProgressBar(widgets=['Cleaning: '] + default_widgets).start()
  files=os.listdir(d)
  if not files: return 0
  cnt,total=0,len(files)
- for cnt,f in enumerate(files): 
-  os.unlink(d+'/'+f)
-  pbar.update(float(cnt)/float(total)*100)
- pbar.finish()
+ for cnt,f in enumerate(files): os.unlink(d+'/'+f)
  return cnt
 
 def check_temp(d):
  if os.path.isdir(d): print 'Deleted %d files in %s' % (clean_directory(d),d)
  else: os.mkdir(d)
 
+def scan_directory(d):
+ """Returns list of all files with path in a directory recursively."""
+ if not d.endswith(os.sep): d+=os.sep
+ rl=[]
+ try: files=os.listdir(d)
+ except: return []
+ for fn in files:
+  rl.append(d + fn)
+  if os.path.isdir(d + fn): rl+=scan_directory(d + fn)
+ return sorted(rl)
+
 check_temp(tDir)
-photoList=filter(lambda x : x[-4:].lower()=='.jpg',katzlib.scan_directory(startDir,False))
+photoList=filter(lambda x : x[-4:].lower()=='.jpg',scan_directory(startDir))
 img=Image.open(photoList[0])
 xc,yc=img.size[0]/2,img.size[1]/2 # center of circle
 rad=min(img.size)/2-min([dvdx,dvdy])/2 # circle radius
-ely=(float(img.size[0])/float(img.size[1]))*0.95 # elyptic factor
+ely=(float(img.size[0])/float(img.size[1]))*0.8 # elyptic factor
 wg = 2*math.pi/len(photoList)
 
 txt='Photos: %d, \n' % len(photoList)
@@ -42,7 +49,7 @@ txt+='Target resolution: %d x %d, \n' % (dvdx,dvdy)
 txt+='Real length: %d seconds, \n' % (len(photoList)/framerate)
 txt+='Circle: r = %d, M = %d / %d, ely = %2.2f, v = %2.3f, wink = %2.2f, \n' % (rad,xc,yc,ely,wg,wink)
 txt+='Temp dir: %s,\n' % tDir
-print txt
+print(txt)
 
 for cnt,f in enumerate(photoList):
  xi,yi=int(rad*math.sin(wink)*ely)+xc,int(rad*math.cos(wink))+yc
@@ -53,3 +60,4 @@ for cnt,f in enumerate(photoList):
  ofn = '%s/%s' % (tDir, os.path.basename(f))
  img.save(ofn)
 
+# ffmpeg -y -hide_banner -loglevel panic -framerate %d -pattern_type glob -i '/home/nyk/florologium_hires_2_rota/*.jpg' -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 test.mp4
