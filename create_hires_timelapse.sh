b319fe01b442b0d0c1637b66cfca44d40b7bd955
nick@nyk.ch <nick@nyk.ch>
2022-09-08 01:05:52 +0200

    Update

--- create_hires_timelapse.sh
+++ create_hires_timelapse.sh
@@ -1,14 +1,14 @@
 Rscript Florologium/timepoint_timelapse_hires.R 2 1
-Florologium/title_generator.py florologium_hires_2 100 "May to September" "01:00 to 03:00" ""
+Florologium/title_generator.py florologium_hires_2 100 "May to September" "Nighttime" ""
 ffmpeg -y -hide_banner -loglevel panic -framerate 20 -pattern_type glob -i 'florologium_hires_2/*.jpg' -s 2048x1080 -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 timelapse2K/florologium_hires_2.mp4
 
 Rscript Florologium/timepoint_timelapse_hires.R 7 1
-Florologium/title_generator.py florologium_hires_7 100 "May to September" "06:00 to 08:00" ""
+Florologium/title_generator.py florologium_hires_7 100 "May to September" "Daytime" ""
 ffmpeg -y -hide_banner -loglevel panic -framerate 20 -pattern_type glob -i 'florologium_hires_7/*.jpg' -s 2048x1080 -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 timelapse2K/florologium_hires_7.mp4
 
 Rscript Florologium/timepoint_timelapse_hires.R 7 0
 Florologium/floro_rotazoom.py florologium_hires_7
-Florologium/title_generator.py florologium_hires_7_rota 100 "May to September" "06:00 to 08:00" "Rotating Version"
+Florologium/title_generator.py florologium_hires_7_rota 100 "May to September" "Daytime" "Rotating Version"
 ffmpeg -y -hide_banner -loglevel panic -framerate 20 -pattern_type glob -i 'florologium_hires_7_rota/*.jpg' -s 2048x1080 -c:v libx264 -strict -2 -pix_fmt yuv420p -f mp4 timelapse2K/florologium_hires_7_rota.mp4
 
 Rscript Florologium/day_timelapse_hires.R 2022-05-10
